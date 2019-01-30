ddfe7724022f5e15dd6ba336ca96bc4b79e90cbc
akonovalov <aleksey.konovalov17@gmail.com>
2019-01-30 18:58:43 +0500

    init project

---
+++ hostels/app/src/main/java/aleksey/projects/hostels/MainActivity.kt
@@ -0,0 +1,12 @@
+package aleksey.projects.hostels
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
